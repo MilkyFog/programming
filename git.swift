0701f3c4586cfb0a3b1c0b59de4b4b90960c840f
Hathaway <2107226736@qq.com>
2023-03-11 13:31:02 +0800

    3.11 git + winget

---
+++ git.swift
@@ -0,0 +1,18 @@
+1. git的作用:
+  1. 
+2. git的使用:
+  1. vscode集成git使用
+    1. 安装git
+    2. 打开项目文件夹
+    3. 初始化Git仓库: git init
+    4. 向git仓库添加所有文件: git addd .
+    5. 提交更改: git commit -m "commit message"
+    6. 查看更改: git status 显示哪些文件已修改但尚未添加到Git仓库中 以及哪些文件已添加到Git仓库但尚未提交
+    7. 连接远程仓库: git remote add origin <remote repository URL>
+    8. 推送更改: git push
+
+  2. 强烈建议所有的git仓库都有一个README, LICENSE, .gitignore文件
+  3. 
+
+
+git的常用且重要操作有哪些
---
+++ winget.swift
@@ -0,0 +1,25 @@
+1. winget的作用
+  1. 帮助用户在Windows操作系统中轻松地安装、升级和管理各种软件包
+  2. 管理多台Windows设备上的软件包安装和升级。
+  3. 在开发环境中轻松地安装和配置所需的工具和框架。
+  4. 在企业环境中自动化软件部署和更新
+2. winget的使用
+  1. winget search node: 搜索想要的软件
+  2. winget install --id __ --source winget: 安装软件
+  3. winget list: 查看当前已经安装的软件
+  4. winget upgrade: 升级所有已经安装的应用程序
+  5. 指定位置下载: 
+    1. mkdir C:\programming\winget
+    2. winget install node -l "C:\programming\winget"
+  6. 设置环境变量
+    1. rundll32 sysdm.cpl,EditEnvironmentVariables
+    2. 在“用户变量”或“系统变量”下 找到名为Path的变量 并单击“编辑”按钮。
+    3. 在弹出的对话框中 单击“新建”按钮。
+    4. C:\programming\winget\node 然后单击“确定”。
+    5. 确认所做更改并关闭对话框。
+    6. 关闭并重新打开Windows终端。
+  7. 查看安装软件的位置
+    1. winget list 找到安装软件的id
+    2. winget show id
+  8. 卸载软件: winget unistall id
+
