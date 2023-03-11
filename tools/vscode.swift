912548eab1385b93bbc870f9112fd6c78333a01f
Hathaway <2107226736@qq.com>
2023-03-11 14:43:26 +0800

    Git learning

---
+++ tools/vscode.swift
@@ -0,0 +1,32 @@
+1. 编辑器
+  1. 黑色的编辑器主题: Community Material Theme
+  2. 让编辑器字体是中文简体: Chinese(Simplified)
+  3. 让报错显示在代码后面: Error Lens
+  4. 让TODO FIXME高亮显示: Better Comments
+  6. 让文件夹好看一点: vscode-icons-mac
+  8. 辅助输入英文: Dictionary Completion
+  9. 方便管理项目: Project Manager
+  10. 格式化代码: Prettier
+  12. 统一风格的代码规范: ESLint
+  // 13. 快速生成注释: Document this
+  14. 代码拼写检查: Code Spell Checker
+  15. 快速跳转TODO注释: TODO Tree
+2. 前端
+  1. 基础
+    1. HTML标签自动闭合: Auto Close Tag
+    2. HTML标签自动重命名: Auto Rename Tag
+    3. 智能提示HTML和CSS代码: HTML CSS Support
+    4. 让十六进制颜色显示出来: Color Highlight
+    5. Ctrl + 左键定位到class的CSS位置: CSS Peek
+    7. 让路径自动补全: Path Intellisense
+  2. Vue: https://zhuanlan.zhihu.com/p/563841068
+    1. 主题颜色和Vue一样: Vue Theme
+    2. 快速生成代码: Vue VSCode Snippets
+      1. 生成模板: vbase vbase-setup
+      2. 生成for: vfor
+    3. 快速跳转到组件模块的文件: Vue Peek
+    4. 让Vue3语法高亮 ts类型检查: Vue Language Features(Volar)
+3. Git
+  1. GitLens: 快速查看代码的编写者
+  2. Git History: 查看Git日志 比较分支和提交
+  3. Git Graph 插件用于可视化查看存储库的 Git 操作 并从图形中轻松执行Git操作
